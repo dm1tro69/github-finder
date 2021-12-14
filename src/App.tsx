96aa7a5047a79cf44ad167770df335cc849e224f
dm1tro69 <dimolg22@gmail.com>
2021-12-14 14:23:07 +0200

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import About from "./pages/About";
 
 
 function App() {
   return (
-    <div>
-       <h1>Hello World</h1>
+    <BrowserRouter>
+        <div className={'flex flex-col justify-between h-screen'}>
+            <Navbar/>
+            <main className={'container mx-auto px-3 pb-12'}>
+                <Routes>
+                    <Route path={'/'} element={<Home/>}/>
+                    <Route path={'/about'} element={<About/>}/>
+                    <Route path={'/*'} element={<NotFound/>}/>
+                </Routes>
+            </main>
+            <Footer/>
+        </div>
 
-    </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,31 @@
+import React, {FC} from 'react';
+import {FaGithub} from "react-icons/fa";
+import {Link} from 'react-router-dom'
+
+type NavbarType = {
+    title?: string
+}
+
+const Navbar:FC<NavbarType> = ({title='Github Finder'}) => {
+    return (
+        <nav className={'navbar mb-12 shadow-lg bg-neutral text-neutral-content'}>
+            <div className={'container mx-auto'}>
+                <div className='flex-none px-2 mx-2'>
+                    <FaGithub className='inline pr-2 text-3xl' />
+                    <Link to='/' className='text-lg font-bold align-middle'>
+                        {title}
+                    </Link>
+                </div>
+                <div className={'flex-1 px-2 mx-2'}>
+                    <div className={'flex justify-end'}>
+                        <Link className={'btn btn-ghost btn-sm rounded-btn'} to={'/'}>Home</Link>
+                        <Link className={'btn btn-ghost btn-sm rounded-btn'} to={'/about'}>About</Link>
+                    </div>
+                </div>
+            </div>
+
+        </nav>
+    );
+};
+
+export default Navbar;
